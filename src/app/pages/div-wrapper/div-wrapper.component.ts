ad0a29655d857a87ed769337670c837eb82da43b
Birame1998 <biramemoussadiouf@gmail.com>
2025-06-16 17:30:01 +0000

    First Commit

--- src/app/pages/div-wrapper/div-wrapper.component.ts
+++ src/app/pages/div-wrapper/div-wrapper.component.ts
@@ -35,31 +35,31 @@ export class DivWrapperComponent {
       id: 4,
       name: "Crispy Fried Chicken",
       price: "$15.10",
-      image: "/assets/shop3-1-202x181-png.png",
+      image: "/assets/riz.png",
     },
     {
       id: 5,
       name: "Zinger Double Burger",
       price: "$20.10",
-      image: "/assets/shop1-1-202x181-png.png",
+      image: "/assets/poisson.jpg",
     },
     {
       id: 6,
       name: "Margherita Pizza New",
       price: "$15.80",
-      image: "/assets/product4-202x181-png-1.png",
+      image: "/assets/griade.webp",
     },
     {
       id: 7,
       name: "Crispy Fried Chicken",
       price: "$10.85",
-      image: "/assets/shop3-1-202x181-png-1.png",
+      image: "/assets/riz.png",
     },
     {
       id: 8,
       name: "Black Pepper Burger",
       price: "$10.85",
-      image: "/assets/shop2-1-202x181-png.png",
+      image: "/assets/poisson.jpg",
     },
   ];
 }
