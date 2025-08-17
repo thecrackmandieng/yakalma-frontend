a5fd2a42e5b5b7e45f1913f32fc259d534d01666
thecrackmandieng <moustaphadieng0405@gmail.com>
2025-08-17 20:32:12 +0000

    comm

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 10000;
 
-// chemin vers dist Angular (attention au sous-dossier "browser" avec Angular 18 SSR/prerender)
-app.use(express.static(path.join(__dirname, "dist/yakalma-frontend/browser")));
+// chemin vers dist Angular
+app.use(express.static(path.join(__dirname, "dist/yakalma-frontend")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist/yakalma-frontend/browser/index.html"));
+  res.sendFile(path.join(__dirname, "dist/yakalma-frontend/index.html"));
 });
 
 app.listen(PORT, () => {
