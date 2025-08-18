8c2daa9eff1971856fd7949eb897df560d3f89e9
thecrackmandieng <moustaphadieng0405@gmail.com>
2025-08-18 21:31:24 +0000

    comm

---
+++ src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,29 @@
+import { Injectable, Inject } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { isPlatformBrowser } from '@angular/common';
+import { PLATFORM_ID } from '@angular/core';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (!isPlatformBrowser(this.platformId)) {
+      return next.handle(req);
+    }
+
+    const token = localStorage.getItem('token') || localStorage.getItem('authToken');
+    
+    if (token) {
+      const authReq = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      return next.handle(authReq);
+    }
+
+    return next.handle(req);
+  }
+}
