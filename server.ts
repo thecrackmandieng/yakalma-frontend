04c6b93ae7c459fa65054c728baa9a5e65d76abf
thecrackmandieng <moustaphadieng0405@gmail.com>
2025-08-18 16:15:55 +0000

    comm

--- server.ts
+++ server.ts
@@ -1,4 +1,5 @@
 import { APP_BASE_HREF } from '@angular/common';
+import { CommonEngine } from '@angular/ssr';
 import express from 'express';
 import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
@@ -10,6 +11,8 @@ export function app(): express.Express {
   const serverDistFolder = dirname(fileURLToPath(import.meta.url));
   const browserDistFolder = resolve(serverDistFolder, '../browser');
   const indexHtml = join(serverDistFolder, 'index.server.html');
+
+  const commonEngine = new CommonEngine();
 
   server.set('view engine', 'html');
   server.set('views', browserDistFolder);
@@ -26,21 +29,16 @@ export function app(): express.Express {
   server.get('**', (req, res, next) => {
     const { protocol, originalUrl, baseUrl, headers } = req;
 
-    // Use Angular SSR rendering
-    import('@angular/ssr').then(({ CommonEngine }) => {
-      const commonEngine = new CommonEngine();
-
-      commonEngine
-        .render({
-          bootstrap,
-          documentFilePath: indexHtml,
-          url: `${protocol}://${headers.host}${originalUrl}`,
-          publicPath: browserDistFolder,
-          providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
-        })
-        .then((html: string) => res.send(html))
-        .catch((err: Error) => next(err));
-    }).catch((err: Error) => next(err));
+    commonEngine
+      .render({
+        bootstrap,
+        documentFilePath: indexHtml,
+        url: `${protocol}://${headers.host}${originalUrl}`,
+        publicPath: browserDistFolder,
+        providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
+      })
+      .then((html: string) => res.send(html))
+      .catch((err: Error) => next(err));
   });
 
   return server;
