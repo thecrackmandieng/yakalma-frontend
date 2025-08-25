bec99d49d5ba24f93135bc626fee589c5c39abe1
thecrackmandieng <moustaphadieng0405@gmail.com>
2025-08-25 23:07:19 +0000

    commm

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,8 @@ export const environment = {
   socketServerUrl: 'https://yakalma.onrender.com',
 
   // Paiement PayTech
+  API_KEY:'e19ac059b2019575877e88c8f9ead3fd568e5e47f9ef9083f383aafdce72c08b',
+  API_SECRET:'f4eea5bf5cbf312b61ad6587dbe9952609da0b3ec28018a547a7e8bb8f2b3097',
   paytechApiUrl: 'https://paytech.sn/api/payment/request-payment',
   notifyUrl: 'https://yakalma.onrender.com/api/payments/notify',
   returnUrl: 'https://yakalma-frontend.onrender.com/payment/success'
